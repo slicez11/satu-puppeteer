9df0444b7e52d2d1bc01947db2b1b09427eb63fc
whatzittoya <whosendall@gmail.com>
2025-09-25 19:15:47 +0700

    config

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
-};
-
-export default nextConfig;
